25c4e3759ee102eead0127a41acbaaeb6ea30edd
snehal Nyayadhish <snehalnyayadhish8@gmail.com>
2020-03-25 15:34:56 +0530

    first commit

---
+++ Event_Mgnt_System/Event_Mgnt_System/Controllers/AdminController.cs
@@ -0,0 +1,135 @@
+﻿using Event_Mgnt_System.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+
+
+namespace Event_Mgnt_System.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        EventDBEntities db = new EventDBEntities();
+            [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(tbl_admin avm)
+        {
+            tbl_admin ad = db.tbl_admin.Where(x => x.ad_username == avm.ad_username && x.ad_password == avm.ad_password).SingleOrDefault();
+            if (ad != null)
+            {
+
+                Session["ad_id"] = ad.ad_id.ToString();
+                return RedirectToAction("Create");
+
+            }
+            else
+            {
+                ViewBag.error = "Invalid username or password";
+
+            }
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            if (Session["ad_id"] == null)
+            {
+                return RedirectToAction("Login");
+            }
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Event cvm,HttpPostedFileBase imgfile)
+        {
+            string path = uploadimgfile(imgfile);
+            if (path.Equals("-1"))
+            {
+                ViewBag.error = "Image could not be uploaded....";
+            }
+            else
+            {
+                Event ev = new Event();
+                ev.Event_Type= cvm.Event_Type;
+                ev.event_image= path;
+          
+               
+                db.Events.Add(ev);
+                db.SaveChanges();
+                return RedirectToAction("ViewEvents");
+            }
+            return View();
+        }
+
+
+        public ActionResult ViewEvents(int? page)
+        {
+            int pagesize = 9, pageindex = 1;
+            pageindex = page.HasValue ? Convert.ToInt32(page) : 1;
+           
+            var list = db.Events.Where(x => x.Status == 0).OrderByDescending(x=>x.Event_ID).ToList();
+          
+            IPagedList<Event> stu = list.ToPagedList(pageindex, pagesize);
+
+
+            return View(stu);
+
+
+
+        }
+
+
+
+
+
+
+        public string uploadimgfile(HttpPostedFileBase file)
+        {
+            Random r = new Random();
+            string path = "-1";
+            int random = r.Next();
+            if (file != null && file.ContentLength > 0)
+            {
+                string extension = Path.GetExtension(file.FileName);
+                if (extension.ToLower().Equals(".jpg") || extension.ToLower().Equals(".jpeg") || extension.ToLower().Equals(".png"))
+                {
+                    try
+                    {
+
+                        path = Path.Combine(Server.MapPath("~/Content/upload"), random + Path.GetFileName(file.FileName));
+                        file.SaveAs(path);
+                        path = "~/Content/upload/" + random + Path.GetFileName(file.FileName);
+
+                        //    ViewBag.Message = "File uploaded successfully";
+                    }
+                    catch (Exception ex)
+                    {
+                        path = "-1";
+                    }
+                }
+                else
+                {
+                    Response.Write("<script>alert('Only jpg ,jpeg or png formats are acceptable....'); </script>");
+                }
+            }
+
+            else
+            {
+                Response.Write("<script>alert('Please select a file'); </script>");
+                path = "-1";
+            }
+
+
+
+            return path;
+        }
+
+    }
+}
---
+++ Event_Mgnt_System/Event_Mgnt_System/Controllers/UserController.cs
@@ -0,0 +1,78 @@
+﻿using Event_Mgnt_System.Models;
+using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Event_Mgnt_System.Controllers
+{
+    public class UserController : Controller
+    {
+        EventDBEntities db = new EventDBEntities();
+
+        // GET: User
+        public ActionResult Index(int ?page)
+        {
+            int pagesize = 9, pageindex = 1;
+            pageindex = page.HasValue ? Convert.ToInt32(page) : 1;
+
+            var list = db.Events.Where(x => x.Status == 0).OrderByDescending(x => x.Event_ID).ToList();
+
+            IPagedList<Event> stu = list.ToPagedList(pageindex, pagesize);
+
+
+            return View(stu);
+            
+        }
+        [HttpGet]
+        public ActionResult Signup()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Signup(Registration uvm)
+        {
+            Registration r = new Registration();
+            r.User_Name = uvm.User_Name;
+            r.Email_ID = uvm.Email_ID;
+            r.Password = uvm.Password;
+            r.ConfirmPassword = uvm.ConfirmPassword;
+            r.City = uvm.City;
+            r.ContactNumber = uvm.ContactNumber;
+            db.Registrations.Add(r);
+            db.SaveChanges();
+          return RedirectToAction("Login");
+        }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Registration avm)
+        {
+           Registration ad = db.Registrations.Where(x => x.User_Name == avm.User_Name && x.Password == avm.Password).SingleOrDefault();
+            if (ad != null)
+            {
+
+                Session["User_ID"] = ad.User_ID.ToString();
+                return RedirectToAction("UserHome");
+
+            }
+            else
+            {
+                ViewBag.error = "Invalid username or password";
+
+            }
+            return View();
+        }
+
+        public ActionResult UserHome()
+        {
+            return View();
+        }
+    }
+}
